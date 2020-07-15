44ecba96f80c1c441a6e9af191ec08d153eeaef2
UnbelievableWang <48404355+UnbelievableWang@users.noreply.github.com>
2020-07-15 19:38:12 +0800

    Add files via upload

---
+++ a.sh
@@ -0,0 +1,5 @@
+sudo apt-get install build-essential
+sudo apt-get install libgl1-mesa-dev libglu1-mesa-dev freeglut3-dev libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
+sudo apt-get install --no-install-recommends libboost-all-dev
+sudo apt-get install libopenblas-dev liblapack-dev libatlas-base-dev
+sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
